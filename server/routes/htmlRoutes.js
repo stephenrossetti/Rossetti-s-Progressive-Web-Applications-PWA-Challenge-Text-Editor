d4acf4ecdca9b0630f329192655a6a0332810ecb
Stevie Rossetti <rossetti.stephen@gmail.com>
2024-03-24 12:01:05 -0500

    Server side comments and review

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,6 +1,9 @@
+// Import node.js path module //
 const path = require('path');
 
+// Export function //
 module.exports = (app) =>
+// Route handler for a GET request. Essentially creates index.html via the noted path when called //
   app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '../client/dist/index.html'))
   );
--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
+// Run express framework //
 const express = require('express');
 
+// Define the port the server will be on //
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve static files that will be rendered from htmlRoutes.js in JSON format //
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Initiate function from htmlRoutes //
 require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
